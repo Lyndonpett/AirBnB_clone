9f1c4fb85427e9100e14c0deaf9eac674f21da77
lyndonpett <lyndonpett98@gmail.com>
2021-10-26 11:34:36 -0500

    Kwargs in init

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,19 @@ from datetime import datetime
 class BaseModel():
     '''Base model for future classes'''
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''init method of BaseModel'''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key in kwargs.keys():
+                if key in ('created_at', 'updated_at'):
+                    kwargs[key] = datetime.strptime(
+                        kwargs[key], '%Y-%m-%dT%H:%M:%S.%f')
+                if key != ('__class__'):
+                    setattr(self, key, kwargs[key])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         '''string representation of the BaseModel'''
