8bed90e206be940ee3f4f9d2ab8758abdc937a96
lyndonpett <lyndonpett98@gmail.com>
2021-10-29 18:20:39 -0500

    Other version of filestorage tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,57 +1,59 @@
 #!/usr/bin/python3
-'''FileStorage Unittests'''
-
+""""Tests the file_storage module"""
 import unittest
-import os
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 
 
 class TestFileStorage(unittest.TestCase):
-    '''FileStorage tests'''
+    """Class tests file_storage class"""
 
     def setUp(self):
-        '''setup before each test'''
-        self.FS = FileStorage()
+        """setup before each test"""
+        self.fs = FileStorage()
 
     def tearDown(self):
-        '''remove file.json at end of test'''
-        if os.path.exists('file.json'):
-            os.remove('file.json')
+        """clean up after each test"""
+        with open("file.json", "w") as f:
+            blankDict = {}
+            f.write("{}".format(blankDict))
 
-    def test_new(self):
-        '''tests self.new()'''
+    def testAll(self):
+        """Test the all method"""
+        self.assertEqual(self.fs.all(), {})
+        a = BaseModel(id=69, created_at="1000-07-29T12:14:07.132263",
+                      updated_at="1020-02-13T07:10:03.134263")
+        self.fs.new(a)
+        self.assertEqual(self.fs.all(), {"BaseModel.69": a})
+
+    def testNew(self):
+        """test the new method"""
         with self.assertRaises(TypeError):
-            self.FS.new()
+            self.fs.new()
 
-    def test_all(self):
-        '''tests self.all()'''
-        thing = BaseModel(id='420', created_at='1999-12-31T11:59:59.999999',
-                          updated_at='1999-12-31T11:59:59.999999')
-        self.FS.new(thing)
-        self.assertEqual(self.FS.all(), {'BaseModel.420': thing})
+    def testSave(self):
+        """tests the save method"""
+        # file created
+        a = BaseModel(id=69, created_at="1000-07-29T12:14:07.132263",
+                      updated_at="1020-02-13T07:10:03.134263")
+        self.fs.new(a)
+        self.fs.save()
+        with open("file.json") as f:
+            line = f.readline()
+            string = ['{"BaseModel.69": {"id": 69, "created_at"',
+                      ': "1000-07-29T12:14:07.132263", "updated_at": ',
+                      '"1020-02-13T07:10:03.134263",',
+                      ' "__class__": "BaseModel"}}']
+            fullstr = string[0] + string[1] + string[2] + string[3]
+            self.assertEqual(line, fullstr)
 
-    def test_save(self):
-        '''tests self.save()'''
-        thing = BaseModel(id='420', created_at='1999-12-31T11:59:59.999999',
-                          updated_at='1999-12-31T11:59:59.999999')
-        self.FS.new(thing)
-        self.FS.save()
-        with open('file.json') as file:
-            ln = file.readline()
-            json_str = ['{"BaseModel.420": {"id": "420", "created_at":',
-                        ' "1999-12-31T11:59:59.999999", "updated_at":',
-                        ' "1999-12-31T11:59:59.999999", "__class__":',
-                        ' "BaseModel"}}']
-        self.assertEqual(ln, "".join(json_str))
-
-    def test_reload(self):
-        '''tests self.reload()'''
-        self.FS.reload()
-        self.assertEqual(self.FS.all(), {})
-        thing = BaseModel(id='420', created_at='1999-12-31T11:59:59.999999',
-                          updated_at='1999-12-31T11:59:59.999999')
-        self.FS.new(thing)
-        self.FS.save()
-        self.FS.reload()
-        self.assertEqual(str(self.FS.all().get("BaseModel.420")), str(thing))
+    def testReload(self):
+        """tests the reload method"""
+        self.fs.reload()
+        self.assertEqual(self.fs.all(), {})
+        a = BaseModel(id=69, created_at="1000-07-29T12:14:07.132263",
+                      updated_at="1020-02-13T07:10:03.134263")
+        self.fs.new(a)
+        self.fs.save()
+        self.fs.reload()
+        self.assertEqual(str(self.fs.all().get("BaseModel.69")), str(a))
