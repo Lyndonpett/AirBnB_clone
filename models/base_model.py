7b641dfd8821ce9ce2c5cea4a78027bb4fb0b9db
treserio <treserio@hotmail.com>
2021-10-30 13:17:10 -0500

    ln 25, run new for all init

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+        models.storage.new(self)
 
     def __str__(self):
         '''string representation of the BaseModel'''
