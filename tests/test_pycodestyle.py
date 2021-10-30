d502625c93bd37ec323b28f2d9a838bebd565eaa
treserio <treserio@hotmail.com>
2021-10-29 23:29:45 -0500

    +pycode testing

---
+++ tests/test_pycodestyle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Test the console module"""
+import unittest
+import pycodestyle
+import os
+
+
+class TestAll(unittest.TestCase):
+    """Tests over-arching things"""
+
+    def setUp(self):
+        """sets up the class"""
+        self.files = ['models/engine/file_storage.py',
+                      'models/amenity.py',
+                      'models/base_model.py',
+                      'models/city.py',
+                      'models/place.py',
+                      'models/review.py',
+                      'models/state.py',
+                      'models/user.py',
+                      'console.py']
+        pass
+
+    def tearDown(self):
+        """cleans up the class"""
+        pass
+
+    def testPycodestyle(self):
+        """Testing pycodestyle validation"""
+        check = pycodestyle.StyleGuide(quiet=True)
+        unittest.result = check.check_files(self.files)
+
+        self.assertEqual(unittest.result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def testShebang(self):
+        """tests for shebang at begining of file"""
+        for item in self.files:
+            with open(item) as f:
+                self.assertEqual(f.readline(), "#!/usr/bin/python3\n",
+                                 "first line needs shebang in {}".format(item))
+
+    def testREADME(self):
+        """tests for non-empty README"""
+        with open("README.md") as f:
+            self.assertNotEqual(len(f.read()), 0, " README is empty")
+        with open("tests/README.md") as f:
+            self.assertNotEqual(len(f.read()), 0, "README is empty")
+
+    def testExecutable(self):
+        for item in self.files:
+            self.assertTrue(os.access(item, os.X_OK),
+                            "File {} is not executable".format(item))
